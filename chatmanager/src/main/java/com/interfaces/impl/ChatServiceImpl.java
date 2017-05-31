6478aa7e0d9842190ebcd985cfb1cfae8b73cdef
Lucas <lucas@maisondunet.com>
2017-05-31 10:01:17 +0200

    update (ChatServer): App + Service + Impl

--- chatmanager/src/main/java/com/interfaces/impl/ChatServiceImpl.java
+++ chatmanager/src/main/java/com/interfaces/impl/ChatServiceImpl.java
@@ -1,7 +1,40 @@
 package com.interfaces.impl;
 
+import com.interfaces.ChatService;
+
 /**
  * @author Leboc Philippe.
  */
-public class ChatServiceImpl {
+public class ChatServiceImpl implements ChatService {
+
+    @Override
+    public Object connect() {
+        return null;
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public Object register() {
+        return null;
+    }
+
+    @Override
+    public boolean joinChannel(String channelId) {
+        return false;
+    }
+
+    @Override
+    public void exitChannel(String channelId) {
+
+    }
+
+    @Override
+    public void say(String channelId, String message) {
+
+    }
+
 }
